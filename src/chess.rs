4fb7aef6024bee93af4152cde9517481d3cf2124
DESKTOP-RFL8FMI\Jonatan <jonatan@padjokej.dev>
2025-07-25 16:11:24 +0200

    rendering pieces

---
+++ src/chess.rs
@@ -0,0 +1,3 @@
+pub fn is_white(piece: i8) -> bool {
+    piece & 8 == 8
+}
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,55 @@ use std::time::{Duration, Instant};
 
 use sdl2::event::Event;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::Texture;
 use sdl2::video::Window;
 use sdl2::{Sdl, VideoSubsystem};
 use sdl2::{keyboard::Keycode, render::Canvas};
+use sdl2::image::LoadTexture;
+use sdl2::image::InitFlag;
 
 extern crate sdl2;
 
 mod vectors;
+mod fen;
+mod chess;
 
-fn main() {
-    let res: (u32, u32) = (640, 480);
+fn main() {    
+    let mut board: Vec<i8> = fen::translate_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR".to_string());
+
+    println!("{:?}", board);
+
+
+    let res: (u32, u32) = (640, 640);
 
     let sdl_context: Sdl = sdl2::init().unwrap();
     let video_subsystem: VideoSubsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
     let window: Window = video_subsystem
         .window("Chess", res.0, res.1)
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    
+    let b_pieces = vec![
+        texture_creator.load_texture("src/images/b_pawn.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/b_bishop.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/b_knight.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/b_rook.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/b_queen.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/b_king.png").ok().unwrap()
+    ];
+    let w_pieces = vec![
+        texture_creator.load_texture("src/images/w_pawn.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/w_bishop.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/w_knight.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/w_rook.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/w_queen.png").ok().unwrap(),
+        texture_creator.load_texture("src/images/w_king.png").ok().unwrap()
+    ];
 
     canvas.clear();
     canvas.present();
@@ -79,10 +108,48 @@ fn main() {
 
                 _ => (),
             }
-            
+
             canvas.set_draw_color(Color::RGB(0, 0, 0));
             canvas.clear();
 
+            for i in 0..64 {
+                let r = Rect::new(
+                    i % 8 * 80,
+                    i / 8 * 80,
+                    80,
+                    80
+                );
+                if i % 2 + (i / 8) % 2 == 1 {
+                    canvas.set_draw_color(Color::RGB(78, 73, 95));
+                } else {
+                    canvas.set_draw_color(Color::RGB(246, 214, 189));
+                }
+                
+                _ = canvas.fill_rect(r);
+            }
+
+            for i in 0..64 {
+                if board[i] != 0 {
+                    let mut texture;
+
+                    if chess::is_white(board[i]){
+                        let index = board[i] as usize - 9;
+                        texture = &w_pieces[index];
+                    } else {
+                        let index = board[i] as usize - 1;
+                        texture = &b_pieces[index];
+                    }
+
+                    let attributes = texture.query();
+                    let src_rect = Rect::new(0, 0, attributes.width, attributes.height);
+
+                    let dest_rect = Rect::new(i as i32 % 8 * 80, i as i32 / 8 * 80, 80, 80);
+
+                    _ = canvas.copy(texture, src_rect, dest_rect);
+                }
+                
+            }
+            println!();
 
             canvas.present();
 
